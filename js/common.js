be8ad1eeaf84df4570cf519047a3a14da52be542
kucheriavii <kucheriavii@gmail.com>
2017-09-04 15:35:50 +0300

    Закінчив роботу з блоком _s_about

--- js/common.js
+++ js/common.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+//popup
+    $(".popup").magnificPopup({type:"image"});
+//animate effects
+    $(".top_text h1").animated("fadeInDown", "fadeOutUp");
+    $(".top_text p, .section_header").animated("fadeInUp", "fadeOutDown");
+
+    $(".animation-1").animated("flipInY", "flipOutY");
+    $(".animation-2").animated("fadeInLeft", "fadeOutLeft");
+    $(".animation-3").animated("fadeInRight", "fadeOutRight");
+    //$(".top_text p").animated("fadeInUp", "fadeOutDon");
+
     function heightDetect(){
         $('.main_head').css('height', $(window).height());
     }
@@ -14,19 +25,23 @@ $(document).ready(function() {
 $(".toggle_mnu").click(function() {
     $(".sandwich").toggleClass("active");
 });
+//Закриваємо меню при кліку на один з його пунктів
 $(".top_mnu ul a").click(function() {
     $(".top_mnu").fadeOut(600);
     $(".top_mnu li a").removeClass("fadeInUp animated");
+    $(".top_text").removeClass("h_opacity");
     $(".sandwich").toggleClass("active");
-});
+}).append("<span>");
 
 $(".toggle_mnu").click(function () {
     if ($(".top_mnu").is(':visible')) {
+        $(".top_text").removeClass("h_opacity");
         $(".top_mnu").fadeOut(600);
-        $(".top_mnu li a").removeClass("fadeInUp animated");
+        $(".top_mnu li a").removeClass("fadeInUp animated");// забираємо клас з анімацією
     } else {
+        $(".top_text").addClass("h_opacity");
         $(".top_mnu").fadeIn(600);
-        $(".top_mnu li a").addClass("fadeInUp animated");
+        $(".top_mnu li a").addClass("fadeInUp animated"); //додаємо анімацію
     }
 });
 /*
